6254efca39bc50ea7d1d13c015958fc9fa758e90
Peter <peteryzufl@gmail.com>
2022-10-10 22:04:18 -0700

    test

--- azureprivatelink.ps1
+++ azureprivatelink.ps1
@@ -2,6 +2,8 @@
 $resourceGroupName = ""
 $storageAccountName = ""
 $vmName = ""
+$privateEndpointConnectionName = "$($storageAccountName)pec1"
+$privateEndpointName = "$($storageAccountName)pe1"
 
 Connect-AzAccount
 Set-AzContext -Subscription $subscriptionId
@@ -11,9 +13,16 @@ $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
 $nic = Get-AzNetworkInterface -ResourceId $vm.NetworkProfile.NetworkInterfaces[0].Id
 $subnet = Get-AzVirtualNetworkSubnetConfig -ResourceId $nic.IpConfigurations[0].Subnet.Id
 
-$pec = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $storageAccount.Id
+$privateEndpointConnection = New-AzPrivateLinkServiceConnection -Name $privateEndpointConnectionName -PrivateLinkServiceId $storageAccount.Id -GroupId "file"
+New-AzPrivateEndpoint -ResourceGroupName $resourceGroupName -Name $privateEndpointName -Location $vm.Location -Subnet $subnet -PrivateLinkServiceConnection $privateEndpointConnection
 
-$tuples = $pec.PrivateEndpoint.Id.Split("/", [System.StringSplitOptions]::RemoveEmptyEntries)
+$privateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $storageAccount.Id
+
+$tuples = $privateEndpointConnection.PrivateEndpoint.Id.Split("/", [System.StringSplitOptions]::RemoveEmptyEntries)
 $privateEndpoint = Get-AzPrivateEndpoint -ResourceGroupName $tuples[3] -Name $tuples[7]
 
 Get-AzPrivateLinkResource -PrivateLinkResourceId $storageAccount.Id
+Get-AzPrivateLinkService -ResourceGroupName $resourceGroupName
+
+Remove-AzPrivateEndpointConnection -ResourceId $privateEndpointConnection.Id -Force
+Remove-AzPrivateEndpoint -ResourceGroupName $privateEndpoint.ResourceGroupName -Name $privateEndpoint.Name -Force
